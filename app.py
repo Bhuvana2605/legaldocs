3f53aeba67b7d1fa710a8b790ef9836ccf4fa4ba
Bhuvana2605 <chbhuvana0505@gmail.com>
2025-09-21 18:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,7 @@ import subprocess
 import sys
 import spacy
 
-# Step 1: Try to load the model, if fails, download and retry once
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"], check=True)
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 
 import openai
@@ -76,6 +71,7 @@ if uploaded_file:
     
 else:
     st.info("Upload a PDF or TXT contract to get started!")
+
 
 
 
